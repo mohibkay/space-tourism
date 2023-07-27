e756e1ea2ccdbdff0b12385d69f4a65d1dd1d47e
mohibkay <mohibk.in@gmail.com>
2023-07-27 23:45:17 +0530

    added brand logo redirection

--- app/components/ui/Navbar.tsx
+++ app/components/ui/Navbar.tsx
@@ -1,10 +1,15 @@
+import Menu from 'components/menu';
 import { Icons } from 'components/utils/Icons';
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
     <nav className='flex justify-between items-center p-6'>
-      <Icons.logo />
-      <Icons.hamburger />
+      <Link href='/'>
+        <Icons.logo />
+      </Link>
+      {/* <Icons.hamburger /> */}
+      <Menu />
     </nav>
   );
 };
